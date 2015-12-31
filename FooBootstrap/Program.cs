e88e8b6bbbf16ebf3d3063e428219b51f9ce7f83
jsakamoto <junichi.jp@gmail.com>
2015-12-31 13:29:29 +0900

    initial commit.

---
+++ FooBootstrap/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        var exePath = Path.Combine(baseDir, "foo.exe");
+        Process.Start(exePath);
+    }
+}
